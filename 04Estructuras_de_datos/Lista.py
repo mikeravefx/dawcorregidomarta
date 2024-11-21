a2e84292f8316360b70869f6f442dccdd9fd2426
mikeravefx <m_pita@hotmail.com>
2024-11-21 13:55:10 +0100

    commit

--- 04Estructuras_de_datos/Lista.py
+++ 04Estructuras_de_datos/Lista.py
@@ -1,8 +1,10 @@
 import random
 filas = 6
 columnas = 7
+min=0
+max=100
 
-listota2 = [[random.randint(0, 9) for i in range(filas)] for j in range(columnas)] #para crear la matriz con número aleatorios
+listota2 = [[random.randint(min,max) for i in range(filas)] for j in range(columnas)] #para crear la matriz con número aleatorios
 
 print(listota2)
 
@@ -11,6 +13,17 @@ lista_impares = [listota2[i][j] for i in range(len(listota2)) for j in range(len
 
 # recorre la lista i j y verifica los numeros pares de lista
 
+pares= 0
+impares=0
+
+for i in range(len(listota2)):
+    for j in range(len(listota2[i])):
+        pares += 1
+    else:
+        impares +=1
+
 print("Lista Pares", lista_pares)
+print("Cuantos Pares", pares)
 print()
 print("Lista impares", lista_impares)
+print("Cuantos impares", impares)
